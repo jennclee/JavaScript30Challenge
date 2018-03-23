2c99bc29234d531d8072b22195e400c27a121c49
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-22 17:28:48 -0700

    Add error handling for non-drum kit keys

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -2,12 +2,16 @@ window.addEventListener('keydown', (e) => {
   let key = e.keyCode;
   let audioEl = document.querySelector(`audio[data-key='${key}']`);
   let keyEl = document.querySelector(`div[data-key='${key}']`);
-  keyEl.classList.add('playing');
-  audioEl.play();
+  if (keyEl !== null) {
+    keyEl.classList.add('playing');
+    audioEl.play();
+  }
 });
 
 window.addEventListener('keyup', (e) => {
   let key = e.keyCode;
   let keyEl = document.querySelector(`div[data-key='${key}']`);
-  keyEl.classList.remove('playing');
+  if (keyEl !== null) {
+    keyEl.classList.remove('playing');
+  }
 });
