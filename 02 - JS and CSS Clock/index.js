b187e7b88ac55f4e1bdc3ad29528d3c2e184bad9
jennclee <8753352+jennclee@users.noreply.github.com>
2018-04-05 17:41:26 -0700

    Implement logic to handle style transition for hands

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,23 +1,38 @@
 const setDate = () => {
   const now = new Date();
+  const secondHand = document.querySelector('.second-hand');
+  const minuteHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
 
   // Seconds hand
   const seconds = now.getSeconds();
   const secondsDegree = ((seconds / 60) * 360) + 90;
-  const secondHand = document.querySelector('.second-hand');
   secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+  if (secondsDegree === 90) {
+    secondHand.style.transition = 'none';
+  } else {
+    secondHand.style.transition = '';
+  }
 
   // Minutes hand
   const minute = now.getMinutes();
   const minuteDegree = ((minute / 60) * 360) + 90;
-  const minuteHand = document.querySelector('.min-hand');
   minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+  if (minuteDegree === 90) {
+    minuteHand.style.transition = 'none';
+  } else {
+    minuteHand.style.transition = '';
+  }
 
   // Hours hand
   const hour = now.getHours();
   const hourDegree = ((hour / 60) * 360) + 90;
-  const hourHand = document.querySelector('.hour-hand');
   hourHand.style.transform = `rotate(${hourDegree}deg)`;
+  if (hourDegree === 90) {
+    hourHand.style.transition = 'none';
+  } else {
+    hourHand.style.transition = '';
+  }
 }
 
 setInterval(setDate, 1000);
